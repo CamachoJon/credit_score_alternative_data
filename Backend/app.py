dd99d70928c772a8c4541878019a9976981a9dce
Jonathan Camacho <jonathan.camacho@epita.fr>
2023-07-28 17:04:34 +0200

    addes docstrings

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,16 @@
+import tempfile
+from PIL import Image as PILImage
+import shap
+import shap_service
+import requests
+from io import BytesIO
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image as platypusImage
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from fastapi.responses import FileResponse
+import io
+import matplotlib.pyplot as plt
 import datetime
 from faker import Faker
 import json
@@ -18,32 +31,26 @@ from Database import Database
 from DataPreparation import DataPreparation
 import sys
 sys.path.append("..")
-import matplotlib.pyplot as plt
-import io
-from fastapi.responses import FileResponse
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image as platypusImage
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.pagesizes import letter
-from io import BytesIO
-import requests
-import shap_service
-import shap
-from PIL import Image as PILImage
-import tempfile
 shap.initjs()
 
 
 # define the app and the base URL
 app = FastAPI()
 
+
 class names(BaseModel):
     key1: str
     key2: str
 
+
 @app.get('/user_data', response_model=List[Dict])
 async def get_user_data():
+    """
+    Fetches user data from the database.
+
+    Returns:
+        List[Dict]: A list of dictionaries representing user data records.
+    """
     db = Database()
     query = "SELECT * FROM [dbo].[USERS]"
     results = db.read(query)
@@ -56,9 +63,19 @@ async def get_user_data():
     # Use FastAPI's jsonable_encoder to convert our data into JSON compatible format
     return jsonable_encoder(results_dict)
 
+
 @app.get('/get_user_info')
 async def get_user_info(name: str = '', lastname: str = ''):
+    """
+    Retrieves user information based on the given name and last name.
 
+    Args:
+        name (str): The first name of the user.
+        lastname (str): The last name of the user.
+
+    Returns:
+        str: JSON string containing the user information.
+    """
     query = f'''SELECT us.[TARGET]
       ,us.[CODE_GENDER]
       ,us.[FLAG_OWN_CAR]
@@ -106,7 +123,7 @@ async def get_user_info(name: str = '', lastname: str = ''):
       INNER JOIN USERS us
       ON ui.ID = us.ID
       WHERE ui.NAME = \'{name}\' AND ui.LASTNAME = \'{lastname}\''''
-    
+
     db = Database()
     results = db.read(query)
 
@@ -120,13 +137,30 @@ async def get_user_info(name: str = '', lastname: str = ''):
     return jsonable_encoder(results_json)
 
 # define the index
+
+
 @app.get("/")
 async def root():
+    """
+    Root endpoint of the API.
+
+    Returns:
+        Dict[str, str]: A dictionary containing a welcome message.
+    """
     return {"message": "This is the root route of the API."}
 
 
 @app.post("/predict")
 async def predict(features: List[Dict[str, Union[str, int, float]]]) -> None:
+    """
+    Predicts the credit risk based on the provided features.
+
+    Args:
+        features (List[Dict[str, Union[str, int, float]]]): List of dictionaries containing user data features.
+
+    Returns:
+        str: JSON string containing the predicted credit risk and user data.
+    """
     db = Database()
     og_df = pd.DataFrame(features)
     og_df = DataPreparation.remove_unnecessary_cols(og_df)
@@ -139,8 +173,22 @@ async def predict(features: List[Dict[str, Union[str, int, float]]]) -> None:
 
     return json_data
 
+
 @app.post("/generate_report")
 async def generate_report(name: str = Form(...), lastname: str = Form(...), imp_f: str = Form(...), cat: str = Form(...), image: UploadFile = File(...)):
+    """
+    Generates a PDF report with credit risk analysis results.
+
+    Args:
+        name (str): The first name of the user.
+        lastname (str): The last name of the user.
+        imp_f (str): Comma-separated string of important features for credit risk analysis.
+        cat (str): Category of the user for credit risk analysis.
+        image (UploadFile): Uploaded image file used in the report.
+
+    Returns:
+        FileResponse: PDF file response with the generated report.
+    """    
     imp_f_list = imp_f.split(",")
 
     # Read the uploaded image file
@@ -148,7 +196,7 @@ async def generate_report(name: str = Form(...), lastname: str = Form(...), imp_
     print(f'Personal data {name}, {lastname}, {cat}')
     # Convert image_content into an Image object
     pil_image = PILImage.open(BytesIO(image_content))
-    
+
     # Save PIL Image to temporary file
     temp_file_name = "temp.png"
     pil_image.save(temp_file_name)
@@ -166,20 +214,23 @@ async def generate_report(name: str = Form(...), lastname: str = Form(...), imp_
     title = Paragraph("Credit Risk Analysis", title_style)
     h1 = Paragraph(f"User: {name} {lastname}", heading_style)
     h1_1 = Paragraph(f"Category: {cat}", heading_style)
-    h2 = Paragraph("Understanding our Credit Risk Analysis System", heading_style)
+    h2 = Paragraph(
+        "Understanding our Credit Risk Analysis System", heading_style)
     p1 = Paragraph("In our system, various factors are ranked based on their significance in determining credit risk. \
                 The middle line represents a neutral point (0.5). \
                 Any value between 0 and 0.5 indicates a lower risk (non-defaulter), and \
                 any value between 0.5 and 1 indicates a higher risk (defaulter).", paragraph_style)
-    h3 = Paragraph("Most Significant Factors in this Risk Assessment: ", heading_style)
-    
+    h3 = Paragraph(
+        "Most Significant Factors in this Risk Assessment: ", heading_style)
+
     # Add the SHAP plot
     story.append(title)
     story.append(h1)
     story.append(h1_1)
     story.append(h2)
     story.append(p1)
-    story.append(platypusImage(temp_file_name, width=300, height=250)) # this line now uses the temporary file
+    # this line now uses the temporary file
+    story.append(platypusImage(temp_file_name, width=300, height=250))
     story.append(Spacer(1, 12))
     story.append(h3)
     for i in range(len(imp_f_list)):
@@ -189,21 +240,37 @@ async def generate_report(name: str = Form(...), lastname: str = Form(...), imp_
 
     # Generate the PDF
     doc.build(story)
-    
+
     # Delete the temporary file
     os.remove(temp_file_name)
 
     # Return the PDF as a response
     return FileResponse("report.pdf", media_type="application/pdf", filename="report.pdf")
 
+
 @app.get('/get_unique_vals')
 async def get_unique_vals():
+    """
+    Retrieves unique values for categorical columns used in credit risk analysis.
+
+    Returns:
+        str: JSON string containing unique values for categorical columns.
+    """
     unique_vals = joblib.load('Model/credit-cat-cols-uniq-vals.joblib')
     json_data = json.dumps(unique_vals, cls=NpEncoder)
     return json_data
 
 
 def prepare_data(features: List[Dict[str, Union[str, int, float]]]) -> pd.DataFrame:
+    """
+    Prepares the user data for credit risk analysis.
+
+    Args:
+        features (List[Dict[str, Union[str, int, float]]]): List of dictionaries containing user data features.
+
+    Returns:
+        pd.DataFrame: Processed DataFrame for credit risk analysis.
+    """
     df = pd.DataFrame(features)
     data_preparation = DataPreparation(df)
     processed_df = data_preparation.prepare_data()
@@ -211,6 +278,15 @@ def prepare_data(features: List[Dict[str, Union[str, int, float]]]) -> pd.DataFr
 
 
 def make_prediction(df: pd.DataFrame) -> List:
+    """
+    Makes predictions for credit risk based on the provided DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing user data features.
+
+    Returns:
+        List: List of predictions (credit risk values).
+    """
     try:
         model = joblib.load('/app/Model/xgb_model.joblib')
         joblib.dump(df, "app/Model/x_test_proc_shap.joblib")
@@ -222,11 +298,32 @@ def make_prediction(df: pd.DataFrame) -> List:
 
 
 def add_target_and_date(df: pd.DataFrame, predictions: List) -> pd.DataFrame:
+    """
+    Adds the target (credit risk predictions) and date to the DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing user data features.
+        predictions (List): List of predictions (credit risk values).
+
+    Returns:
+        pd.DataFrame: DataFrame with added target and date columns.
+    """
     df.insert(0, 'TARGET', predictions)
     df['DATE'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return df
 
+
 def write_to_db(df: pd.DataFrame, db: Database) -> None:
+    """
+    Writes user analysis and user info to the database.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing user data features and credit risk predictions.
+        db (Database): Database connection object.
+
+    Returns:
+        None
+    """
     dict_list = df.to_dict(orient='records')
     for record in dict_list:
         user_analysis_query = Database.format_sql_command('USERS', record)
@@ -237,13 +334,22 @@ def write_to_db(df: pd.DataFrame, db: Database) -> None:
 
 
 def generate_fake_data(user_id):
+    """
+    Generates fake user data.
+
+    Args:
+        user_id: User ID.
+
+    Returns:
+        dict: Dictionary containing fake user data (name, lastname, birthdate).
+    """
     fake = Faker()
     fake_name = fake.name()
     fake_lastname = fake.last_name()
     fake_birthdate = fake.date_of_birth().strftime("%Y-%m-%d")
 
     data_dict = {
-        'ID' : user_id,
+        'ID': user_id,
         'NAME': fake_name,
         'LASTNAME': fake_lastname,
         'BIRTHDATE': fake_birthdate
@@ -252,6 +358,12 @@ def generate_fake_data(user_id):
 
 
 class NpEncoder(json.JSONEncoder):
+    """
+    Custom JSON encoder for NumPy data types.
+
+    This class is used to ensure JSON serialization of NumPy data types.
+
+    """
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
@@ -266,12 +378,18 @@ class NpEncoder(json.JSONEncoder):
 
 @app.get("/shap")
 async def get_shap_values():
+    """
+    Retrieves SHAP (SHapley Additive exPlanations) values for the credit risk analysis model.
+
+    Returns:
+        str: JSON string containing SHAP values, expected value, x_test, class_0, and class_1.
+    """
     shap_values, expected_value, x_test_processed, class_0, class_1 = shap_service.create_explainer()
     sv = shap_values.tolist()
     ev = expected_value.tolist()
     xpr = x_test_processed.to_dict(orient='records')
     pr_df = json.dumps(xpr)
-    
+
     jd = {
         "shap_val": sv,
         "exp_val": ev,
